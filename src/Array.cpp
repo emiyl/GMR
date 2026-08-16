5e360c9a7f899d65b646c407412758f1921353c3
emiyl <me@emiyl.com>
2026-08-16 22:11:18 +0100

    update

--- src/Array.cpp
+++
@@ -1,7 +0,0 @@
-void spFloatArray_create() { return; }
-void spFloatArray_add() { return; }
-void spIntArray_create() { return; }
-void spShortArray_create() { return; }
-void spShortArray_add() { return; }
-void spArrayFloatArray_create() { return; }
-void spArrayShortArray_create() { return; }
