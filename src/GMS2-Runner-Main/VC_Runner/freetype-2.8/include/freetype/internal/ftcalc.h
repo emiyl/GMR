f618ea22dcf267048fbf473d50362e8a74debc3c
emiyl <me@emiyl.com>
2026-08-24 22:00:44 +0100

    add freetype 2.8

--- src/GMS2-Runner-Main/VC_Runner/freetype-2.8/include/freetype/internal/ftcalc.h
+++
@@ -1,4 +0,0 @@
-// Referenced by object 'D:\a\GameMaker\GameMaker\GameMaker\Runner\GMS2-Runner-Main\VC_Runner\x64\Release-Zeus\Font_Class.obj' but no function
-// attribution is possible for included/header files
-// at object-level granularity.
-// Original path: D:\a\GameMaker\GameMaker\GameMaker\Runner\GMS2-Runner-Main\VC_Runner\freetype-2.8\include\freetype\internal\ftcalc.h
